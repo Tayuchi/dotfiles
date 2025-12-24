087edb25d872449f6d8d28b1af89fa84cd47bd0c
tayuchi <tayunotfound@gmail.com>
2025-12-25 06:05:41 +0900

    fix: ウィンドウの起動時設定最大化

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -5,8 +5,7 @@ local mux = wezterm.mux
 -- 起動時のウィンドウサイズと位置
 wezterm.on('gui-startup', function(cmd)
   local tab, pane, window = mux.spawn_window(cmd or {width=240, height=65})
-  window:gui_window():set_position(85,0)
-  window:set_inner_size(1000, 1000)
+  window:gui_window():maximize()
 end)
 
 return {
