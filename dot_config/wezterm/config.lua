4210565c3d9e2bf8007f8dc56edb0e196a24392a
tayuchi <tayunotfound@gmail.com>
2026-01-01 22:23:32 +0900

    feat: ペインを分けるキーバインド追加

--- dot_config/wezterm/config.lua
+++ dot_config/wezterm/config.lua
@@ -9,15 +9,17 @@ wezterm.on('gui-startup', function(cmd)
   window:gui_window():maximize()
 end)
 
+--------------------------------------------------------------------------------
 -- 基本設定
+--------------------------------------------------------------------------------
+-- 自動リロード設定
 config.automatically_reload_config = true
-
 -- フォントとカラースキームの設定
 config.font = wezterm.font("BitstromWera Nerd Font")
 config.font_size = 12.0
 config.color_scheme = "Kanagawa (dragon)"
 config.macos_window_background_blur = 10
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.9
 config.text_background_opacity = 0.5
 config.colors = {
   background = "#181616",    -- 温かみのある暗色
@@ -27,11 +29,20 @@ config.colors = {
   cursor_border = "#DCD7BA", -- 枠線も同じ色で自然に
 }
 
--- キーバインド設定
+--------------------------------------------------------------------------------
+-- キーバインド
+--------------------------------------------------------------------------------
 config.keys = {
+  -- フルスクリーン切替
   { key = "f", mods = "SHIFT|META", action = act.ToggleFullScreen }, -- Shift+Cmd+F
   { key = 'f', mods = 'CTRL|CMD', action = wezterm.action.ToggleFullScreen }, -- Ctrl+Cmd+F
+
+  -- ペイン分割
+  { key = 'd', mods = 'CMD', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } }, -- Cmd+D: 左右に分割
+  { key = 'd', mods = 'CMD|SHIFT', action = act.SplitVertical { domain = 'CurrentPaneDomain' } }, -- Cmd+Shift+D: 上下に分割
+
+  -- ペインを閉じる
+  { key = 'w', mods = 'CMD', action = act.CloseCurrentPane { confirm = true } }, -- Cmd+W: ペインを閉じる
 }
 
 return config
-
